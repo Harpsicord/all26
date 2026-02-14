6f240a689b0394a9d23d517ca0e40cdc865691f9
Harpsicord <harperangelows@gmail.com>
2026-02-14 11:56:45 -0800

    NO MORE ERRORS!!!

--- comp/src/main/java/org/team100/frc2026/ClimberExtension.java
+++ comp/src/main/java/org/team100/frc2026/ClimberExtension.java
@@ -17,81 +17,88 @@ import org.team100.lib.profile.r1.IncrementalProfile;
 import org.team100.lib.profile.r1.TrapezoidIncrementalProfile;
 import org.team100.lib.reference.r1.IncrementalProfileReferenceR1;
 import org.team100.lib.reference.r1.ProfileReferenceR1;
+import org.team100.lib.sensor.position.absolute.sim.SimulatedRotaryPositionSensor;
 import org.team100.lib.sensor.position.incremental.IncrementalBareEncoder;
+import org.team100.lib.servo.AngularPositionServo;
+import org.team100.lib.servo.LinearPositionServo;
+import org.team100.lib.servo.OnboardAngularPositionServo;
+import org.team100.lib.servo.OutboardAngularPositionServo;
 import org.team100.lib.servo.OutboardLinearPositionServo;
 import org.team100.lib.util.CanId;
-
-import com.ctre.phoenix6.swerve.utility.LinearPath;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberExtension extends SubsystemBase {
-     private final BareMotor m_motor;
+    private final LinearPositionServo m_servo;
 
-     private final double m_maxExtension=20;
+    private final double m_maxExtension = 20;
 
     public ClimberExtension(LoggerFactory parent) {
         LoggerFactory log = parent.type(this);
-        
+        IncrementalProfile profile = new TrapezoidIncrementalProfile(log, 1, 2, 0.05);
+        ProfileReferenceR1 ref = new IncrementalProfileReferenceR1(log, () -> profile, 0.05, 0.05);
+
         switch (Identity.instance) {
             case COMP_BOT -> {
-                  
-                m_motor = new KrakenX60Motor(
-                    log,
-                    new CanId(0),
-                    NeutralMode100.BRAKE, 
-                    MotorPhase.FORWARD, 
-                    60, 1,
-                    new SimpleDynamics(log, 0, 0),
-                    new Friction(log, 0, 0, 0, 0),
-                    new PIDConstants(log, 0, 0, 0, 0, 0, 0));
-                    IncrementalBareEncoder encoder = m_motor.encoder();
 
-                TrapezoidIncrementalProfile profile = new TrapezoidIncrementalProfile(log, 1, 2, 0.05);
-                ProfileReferenceR1 ref = new IncrementalProfileReferenceR1(log, ()-> profile, 0.05);
+                KrakenX60Motor m_motor = new KrakenX60Motor(log, new CanId(0), NeutralMode100.BRAKE, MotorPhase.FORWARD,
+                        60, 1,
+                        new SimpleDynamics(log, 0, 0), new Friction(log, 0, 0, 0, 0),
+                        new PIDConstants(log, 0, 0, 0, 0, 0, 0));
+
+                IncrementalBareEncoder encoder = m_motor.encoder();
+
                 double initialPosition = 0;
                 double gearRatio = 2;
-                 m_servo = new OutboardLinearPositionServo( log, climberMech, ref, feedback );
-                                LinearPath climberMech = new LinearPath(
-                                        log, m_servo, encoder, initialPosition, gearRatio,
+                LinearMechanism climberMech = new LinearMechanism(
+                        log, m_motor, encoder, initialPosition, gearRatio,
                         Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-            }:
-                break;
+                m_servo = new OutboardLinearPositionServo(
+                       log, // LoggerFactory parent,
+                        climberMech,// LinearMechanism mechanism,
+                       ref, // ProfileReferenceR1 ref,
+                       0.01, // double positionTolerance,
+                       0.01 // double velocityTolerance)
+                );
+            }
 
             default -> {
-                m_motor = new SimulatedBareMotor(log, 600);
-                IncrementalProfile profile = new TrapezoidIncrementalProfile(log, 1, 2, 0.05);
-                ProfileReferenceR1 ref = new IncrementalProfileReferenceR1(log, profile, 0.05, 0.05);
+                SimulatedBareMotor m_motor = new SimulatedBareMotor(log, 600);
                 PIDFeedback feedback = new PIDFeedback(log, 5, 0, 0, false, 0.05, 0.1);
                 IncrementalBareEncoder encoder = m_motor.encoder();
 
-                SimulatedLinearPositionSensor sensor =
-                new SimulatedLinearPositionSensor(log, encoder, 1);
-                RotaryMechanism climberMech =
-                new LinearMechanism(
-                    log,
-                    m_motor,
-                     sensor,
-                    1,
-                     Double.NEGATIVE_INFINITY,
-                     Double.POSITIVE_INFINITY);
-                m_servo = new OnboardLinearPositionServo( log, climberMech, ref, feedback );
+                SimulatedRotaryPositionSensor sensor = new SimulatedRotaryPositionSensor(log, encoder, 1);
+                Double minimumPosition = 0.1;
+                LinearMechanism climberMech = new LinearMechanism(
+                        log, m_motor, encoder, 1, 2,
+                        Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+                m_servo = new OutboardLinearPositionServo(
+                  log,  // LoggerFactory parent,
+                   climberMech, // LinearMechanism mechanism,
+                   ref, // ProfileReferenceR1 ref,
+                  0.01,  // double positionTolerance,
+                  0.01  // double velocityTolerance
+                );
             }
-         }
+        }
     }
-    
+
     public Command setPosition() {
         return run(this::setOutPosition);
     }
+
     public Command setHomePosition() {
         return run(this::setInPosition);
     }
+
     private void setOutPosition() {
-        m_motor.setUnwrappedPosition(m_maxExtension,0,0,0);
-    
+        m_servo.setPositionProfiled(m_maxExtension, 0);
+
     }
+
     public void setInPosition() {
-        m_motor.setUnwrappedPosition(0,0,0,0);
+        m_servo.setPositionProfiled(0, 0);
     }
 }
